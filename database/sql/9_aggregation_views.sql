0ca0d54af71f0ad4f25ce87bed1fb9da948e5fb7
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-02-04 17:24:12 +0100

    database: Create aggregation views in new script 9_aggregation_views.sql

---
+++ database/sql/9_aggregation_views.sql
@@ -0,0 +1,52 @@
+-- Add index on cds_cdm.observation.observation_datetime
+-- May speed up the aggregation views defined below
+CREATE INDEX idx_observation_observation_datetime_1
+ON cds_cdm.observation (observation_datetime);
+
+-- Daily aggregation
+CREATE VIEW cds_cdm.quality_indicator_daily_average AS
+SELECT   observation_concept_id,
+         DATE_TRUNC('day', observation_datetime)                    AS period_start,
+         DATE_TRUNC('day', observation_datetime) + INTERVAL '1 day' AS period_end,
+         AVG(value_as_number) AS average_value,
+         COUNT(*) AS observation_count
+FROM     cds_cdm.observation
+WHERE    observation_concept_id >= 2000000100 -- only quality indicator observations
+GROUP BY observation_concept_id,
+         DATE_TRUNC('day', observation_datetime);
+
+-- Weekly aggregation
+CREATE VIEW cds_cdm.quality_indicator_weekly_average AS
+SELECT   observation_concept_id,
+         DATE_TRUNC('week', observation_datetime)                     AS period_start,
+         DATE_TRUNC('week', observation_datetime) + INTERVAL '1 week' AS period_end,
+         AVG(value_as_number) AS average_value,
+         COUNT(*) AS observation_count
+FROM     cds_cdm.observation
+WHERE    observation_concept_id >= 2000000100 -- only quality indicator observations
+GROUP BY observation_concept_id,
+         DATE_TRUNC('week', observation_datetime);
+
+-- Monthly aggregation
+CREATE VIEW cds_cdm.quality_indicator_monthly_average AS
+SELECT   observation_concept_id,
+         DATE_TRUNC('month', observation_datetime)                      AS period_start,
+         DATE_TRUNC('month', observation_datetime) + INTERVAL '1 month' AS period_end,
+         AVG(value_as_number) AS average_value,
+         COUNT(*) AS observation_count
+FROM     cds_cdm.observation
+WHERE    observation_concept_id >= 2000000100 -- only quality indicator observations
+GROUP BY observation_concept_id,
+         DATE_TRUNC('month', observation_datetime);
+
+-- Yearly aggregation
+CREATE VIEW cds_cdm.quality_indicator_yearly_average AS
+SELECT   observation_concept_id,
+         DATE_TRUNC('year', observation_datetime)                     AS period_start,
+         DATE_TRUNC('year', observation_datetime) + INTERVAL '1 year' AS period_end,
+         AVG(value_as_number) AS average_value,
+         COUNT(*) AS observation_count
+FROM     cds_cdm.observation
+WHERE    observation_concept_id >= 2000000100 -- only quality indicator observations
+GROUP BY observation_concept_id,
+         DATE_TRUNC('year', observation_datetime);
