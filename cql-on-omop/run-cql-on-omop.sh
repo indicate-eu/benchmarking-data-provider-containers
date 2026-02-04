e4a033c596eacda14739efd924fd4c3cec6193fe
Jan Moringen <jan.moringen@med.uni-greifswald.de>
2026-02-04 15:08:40 +0100

    cql-on-omop: Do not print notes for individual persisted objects

--- cql-on-omop/run-cql-on-omop.sh
+++ cql-on-omop/run-cql-on-omop.sh
@@ -55,31 +55,32 @@ REVIEW_PERIOD="Interval[${PREVIOUS_RUN_TIMESTAMP}, ${NOW})"
 # TODO(moringenj): should not use --password for target db
 # TODO(moringenj): MIMIC is temporary
 CQL_ON_OMOP_DATABASE_PASSWORD=$(cat /run/secrets/source-database-password) \
-  java -Xmx24000000000                                      \
-    -jar cql-on-omop-${CQL_ON_OMOP_VERSION}.jar             \
-    batch                                                   \
-      --omop-version=v5.4.MIMIC \
-      ${SOURCE_DB_CONNECTION_STRING_ARG}                    \
-      ${SOURCE_DB_DRIVER_ARG}                               \
-      --host="${SOURCE_DB_HOST}"                            \
-      --port="${SOURCE_DB_PORT}"                            \
-      --user="${SOURCE_DB_USER}"                            \
-      --database="${SOURCE_DB_DATABASE}"                    \
-      --schema="${SOURCE_DB_SCHEMA}"                        \
-      -I cql/                                               \
-      --context-value "${CQL_CONTEXT}"                      \
-      -D"IndicateQiElements.Review Period=${REVIEW_PERIOD}" \
-      --result-name='Results'                               \
-      Main                                                  \
-      dbwrite                                               \
-        ${TARGET_DB_CONNECTION_STRING_ARG}                  \
-        ${TARGET_DB_DRIVER_ARG}                             \
-        --host="${TARGET_DB_HOST}"                          \
-        --port="${TARGET_DB_PORT}"                          \
-        --user="${TARGET_DB_USER}"                          \
-        --password="$(cat /run/secrets/target-database-password)" \
-        --database="${TARGET_DB_DATABASE}"                  \
-        --schema="${TARGET_DB_SCHEMA}"
+  java -Xmx24000000000                                                     \
+    -jar cql-on-omop-${CQL_ON_OMOP_VERSION}.jar                            \
+    batch                                                                  \
+    --omop-version=v5.4.MIMIC \
+      ${SOURCE_DB_CONNECTION_STRING_ARG}                                   \
+      ${SOURCE_DB_DRIVER_ARG}                                              \
+      --host="${SOURCE_DB_HOST}"                                           \
+      --port="${SOURCE_DB_PORT}"                                           \
+      --user="${SOURCE_DB_USER}"                                           \
+      --database="${SOURCE_DB_DATABASE}"                                   \
+      --schema="${SOURCE_DB_SCHEMA}"                                       \
+      -I cql/                                                              \
+      --context-value "${CQL_CONTEXT}"                                     \
+      -D"IndicateQiElements.Review Period=${REVIEW_PERIOD}"                \
+      --result-name='Results'                                              \
+      Main                                                                 \
+      dbwrite                                                              \
+        ${TARGET_DB_CONNECTION_STRING_ARG}                                 \
+        ${TARGET_DB_DRIVER_ARG}                                            \
+        --host="${TARGET_DB_HOST}"                                         \
+        --port="${TARGET_DB_PORT}"                                         \
+        --user="${TARGET_DB_USER}"                                         \
+        --password="$(cat /run/secrets/target-database-password)"          \
+        --database="${TARGET_DB_DATABASE}"                                 \
+        --schema="${TARGET_DB_SCHEMA}"                                     \
+        --no-print-notes
 
 # Insert marker for this run
 sql_in_target_db \
